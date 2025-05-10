9f4bba1569b9abd74f5bbfe009b41e18897d6ba4
DTP-03 <6351030059@st.utc2.edu.vn>
2025-05-10 20:05:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -101,4 +101,6 @@ def manual_result():
     return jsonify({"status": result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Lấy port từ Render (hoặc mặc định 5000)
+    app.run(debug=True, host="0.0.0.0", port=port)
+
