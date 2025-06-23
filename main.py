84f90fcf4e85500b5f85d8912e9de483226b92ff
DTP-03 <6351030059@st.utc2.edu.vn>
2025-06-23 19:35:48 +0700

    Update main.py
    sửa lỗi đổi dbSQL v2

--- main.py
+++ main.py
@@ -91,5 +91,7 @@ def stats():
 
 # --- Run ---
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=5000)
+
